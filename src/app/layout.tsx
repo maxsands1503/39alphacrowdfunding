52e04f8af79744b857f1307eafc9e2d0c07702c8
Max Sands <maxsands1503@icloud.com>
2024-10-17 11:32:34 -0600

    working on the side nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={'h-screen'}>
       <body className={ jetBrainsMono.className }>
-        <NavBar />
-        {children}
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
+            <NavBar />
+          </div>
+          <div className="grow p-6 md:overflow-y-auto md:p-12">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
