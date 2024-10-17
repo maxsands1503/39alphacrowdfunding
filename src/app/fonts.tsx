0ccb4e91615c3257072ba4df3d039e1b23f8377f
Max Sands <maxsands1503@icloud.com>
2024-10-17 07:57:30 -0600

    first

---
+++ src/app/fonts.tsx
@@ -0,0 +1,5 @@
+import {JetBrains_Mono} from 'next/font/google';
+
+export const jetBrainsMono = JetBrains_Mono({
+    subsets: ['latin']
+});
---
+++ src/app/navbar.tsx
@@ -0,0 +1,8 @@
+export default function NavBar() {
+    return (
+        <div>
+            fucking navbar
+        </div>
+    )
+
+}
