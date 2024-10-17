ce4aaa322494ec4f7fae295a314959af40d668a8
Max Sands <maxsands1503@icloud.com>
2024-10-17 15:44:18 -0600

    moving to other computer, because couch is nice

--- src/seed/projects.tsx
+++ src/seed/projects.tsx
@@ -1,5 +1,11 @@
-export class Project {
-    public
+export interface Project {
+    title: string;
+    desciption: string;
+    tags: string[];
+    contributors: string[];
+    fundingGoal: number;
+    fundsRaised: number;
+    
 }
 
 export const projects: Project[] = [{
